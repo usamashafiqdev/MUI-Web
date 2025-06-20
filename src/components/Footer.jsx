33bf857f81b1f01aaaeb649772b6b822c7e7806c
usamashafiqdev <itusamashafiq@gmail.com>
2025-06-20 22:52:35 +0500

    Add a Button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-    <div>This section is define about the footer of the Website</div>
+    <div>Footer
+      <button>  Click me!</button>
+    </div>
+    
   )
 }
 
